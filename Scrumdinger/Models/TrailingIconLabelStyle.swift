d7734fd0fefce0d0e760ec3cbbd03ac82d5951f5
Joshua David Ang <joshuadavidang@outlook.sg>
2022-06-13 21:29:20 +0800

    code refactored

---
+++ Scrumdinger/Models/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Joshua David Ang on 13/6/22.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    fields func makeBody(configuration: Configuration) -> some View {
+        HStack() {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
