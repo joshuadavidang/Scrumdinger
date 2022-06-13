3b6a7d196d372227115b8bede540cc1425d773c5
Joshua David Ang <joshua.ang@blockchainsg.com>
2022-06-13 20:08:12 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Joshua David Ang on 13/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
