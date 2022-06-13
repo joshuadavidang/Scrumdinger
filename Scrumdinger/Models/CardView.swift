d89f76130c1b918d239bc1bc42c9add1cfca98ff
Joshua David Ang <joshuadavidang@outlook.sg>
2022-06-13 20:58:43 +0800

    test commit

---
+++ Scrumdinger/Models/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Joshua David Ang on 13/6/22.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
